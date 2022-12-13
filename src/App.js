fc332359208c2ac7779b68b3e3a259c8a39a268a
ujjwal5297 <ujjwalsharma5297@gmail.com>
2022-12-13 12:47:13 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Brand, CTA, Navbar} from './components';
+import {Blog, Features, Footer, Header, Possibility, WhatGPT3} from './containers';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient_bg'>
+      <Navbar/>
+      <Header/>
+
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './brand.css';
+import { google, slack, atlassian, dropbox, shopify,} from './imports';
+
+
+const Brand = () => {
+  return (
+    <div className= 'gpt__brand section__padding'>
+      <div>
+        <img src={google} alt='google'/>
+      </div>
+      <div>
+        <img src={slack} alt='google'/>
+      </div>
+      <div>
+        <img src={atlassian} alt='google'/>
+      </div>
+      <div>
+        <img src={dropbox} alt='google'/>
+      </div>
+      <div>
+        <img src={shopify} alt='google'/>
+      </div>
+    </div>
+  )
+}
+
+export default Brand
